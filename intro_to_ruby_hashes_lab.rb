af27c84c28c3011890f5c3569ceed4c3d05be5e6
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-08 22:50:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,13 @@
 def base_hash
-  hash = {
+  base_hash = {
     :railroads => {}
   }
-  hash
+  
 
 end
 
 def monopoly_with_second_tier
- hash = {
+ base_hash = {
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {},
@@ -15,7 +15,6 @@ def monopoly_with_second_tier
     }
  }
 
-hash
 end
 
 def monopoly_with_third_tier
@@ -23,9 +22,9 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
-      :one_piece_owned => 25
-      :two_pieces_owned => 50
-      :three_pieces_owned => 100
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 100,
       :four_pieces_owned => 200
         
       },
